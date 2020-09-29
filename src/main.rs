ba6e29cc78008e653605ed9300d99081b9dfafbd
LIRUI <lir@pcl.ac.cn>
2020-09-29 23:15:26 +0800

    add comment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//2020-09-29
+/*
+ * Author：LIRUI
+ * E-mail：lir@pcl.ac.cn
+ * 功能实现：在main()中周期性对标志位进行周期性翻转，置0置1
+ *
+*/
+
 pub fn delay(mut count:i32) {
     while count > 0 {
         count = count-1;
